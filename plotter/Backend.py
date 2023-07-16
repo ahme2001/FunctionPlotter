a07e6c8735ed928827f1ce750f9e911ad82bf03c
ahme2001 <ahmedaboeleid30@gmail.com>
2023-07-16 05:02:54 +0300

    implement methods for validation expression and get-points of plot

--- plotter/Backend.py
+++ plotter/Backend.py
@@ -1,6 +1,37 @@
 def validate_expression(expression):
-    return
+    valid_characters = '0123456789+-*/^x'
+    last_character = ''
+    expression = expression.replace(" ", "")
+    expression = expression.replace("+-", "-")
+    expression = expression.replace("--", "+")
+
+    for character in expression:
+        if character not in valid_characters:
+            return False, expression
+        if character in '+*/^' and last_character in '+-*/^':
+            return False, expression
+        if character == 'x' and last_character in '0123456789x' and last_character not in '':
+            expression = expression.replace(last_character + 'x', last_character + '*x')
+        last_character = character
+    if last_character in '+-*/^':
+        return False , expression
+    expression = expression.replace("^", "**")
+
+    return True, expression
 
 def get_points(function, min , max):
-    return
+    x_axis = []
+    y_axis = []
+    for i in range(100):
+        x = min + (max - min) * i / 100
+        x_axis.append(x)
+        try:
+            result = eval(function)
+            if isinstance(result, (int, float)):
+                y_axis.append(result)
+            else:
+                y_axis.append(float('nan'))
+        except ZeroDivisionError:
+            y_axis.append(float('inf'))
+    return x_axis, y_axis
 
